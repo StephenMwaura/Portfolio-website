58f898817fdba6525799e888c9c4e3ba6b1a151c
Stephen Mwaura <stevecartel44@gmail.com>
2026-07-31 14:45:28 +0300

    fixed the views file

--- portfolio/views.py
+++ portfolio/views.py
@@ -65,4 +65,4 @@ def contact(request):
         form = ContactForm()
 
     context = {"form": form}
-    return render(request, "portfolio/contact.html", context)
+    return render(request, "portfolio/contacts.html", context)
